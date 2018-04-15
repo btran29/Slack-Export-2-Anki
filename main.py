755ee98c47c367df1cbb6171f794f0797db2ff6a
Brian <btran29@gmail.com>
2018-04-15 01:52:00 -0700

    formatting

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import csv
 import os
 
 # settings
-output_csv = open('output.csv', 'w')
+output_csv = open('output.csv', 'w', newline='')
 source_directory = r'C:\Users\Brian\Documents\GitHub\Slack-Export-2-Anki\Messages'
 
 
 # Convert default json text field into front and and back, based on delimiter in slack message
 def parse_slack_text(input_text, csv_delimiter):
-    output_dict = {}
+    output_dictionary = {}
     if csv_delimiter in input_text:
         # due to there being multiple usages of the default delimiter, ':'
-        parsed = item['text'].replace(csv_delimiter, "|", 1)  # first find only
+        parsed = message['text'].replace(csv_delimiter, "|", 1)  # first find only
 
         # remove bold formatting from first bolded set of words
         parsed = parsed.replace("*", "", 2)
@@ -23,34 +23,34 @@ def parse_slack_text(input_text, csv_delimiter):
 
         # split into dictionary value
         parsed = parsed.split('|')
-        output_dict["back"] = parsed[0]
-        output_dict["front"] = parsed[1]
+        output_dictionary["back"] = parsed[0]
+        output_dictionary["front"] = parsed[1]
         print(parsed)
     else:
-        # if no delimiter, just map entire text to back
-        output_dict["back"] = item['text']
-        output_dict["front"] = 'Empty'
-    return output_dict
+        # if no delimiter in message, just map entire text to back
+        output_dictionary["back"] = message['text']
+        output_dictionary["front"] = 'Empty'
+    return output_dictionary
 
 
-# Travel through directory
+# Write to CSV
+def write_to_csv(data_in, csv_out):
+    with csv_out as output_file:
+        keys = data_in[0].keys()
+        writer = csv.DictWriter(output_file, fieldnames=keys)
+        writer.writerows(data_in)
+        csv_out.close()
+    return
+
+# Main method
 files = os.listdir(source_directory)
-
-# Declare a list to store parsed text
-output_list = []
+parsed_list = []    # To store parsed text across files
 
 for file in files:
     current_path = os.path.join(source_directory, file)
-    data = json.load(open(current_path))
+    current_slack_file = json.load(open(current_path))
 
-    for item in data:
-        output_list.append(parse_slack_text(item['text'], ':'))
+    for message in current_slack_file:
+        parsed_list.append(parse_slack_text(message['text'], ':'))
 
-
-# write to CSV
-with open('output.csv', 'w', newline='') as output_file:
-    keys = output_list[0].keys()
-    writer = csv.DictWriter(output_file, fieldnames=keys)
-    writer.writerows(output_list)
-    output_csv.close()
-
+write_to_csv(parsed_list, output_csv)
